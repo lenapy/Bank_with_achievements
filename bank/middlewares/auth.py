ebf6ac0d30074a8d11264c6b73c9b42e6677f1e5
lenapy <rashchyk1351@gmail.com>
2017-03-17 00:21:21 +0200

    change auth with no_redirect_urls list

--- bank/middlewares/auth.py
+++ bank/middlewares/auth.py
@@ -8,8 +8,9 @@ class Auth:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.get_full_path() == '/user/login/':
-            response = self.get_response(request)
+        response = self.get_response(request)
+        no_redirect_urls = ['/user/login/', '/user/registration/']
+        if request.get_full_path() in no_redirect_urls:
             return response
         try:
             user_session = request.COOKIES['user-session']
@@ -18,6 +19,5 @@ class Auth:
                 return redirect('user:login')
         except KeyError:
             return redirect('user:login')
-        response = self.get_response(request)
         return response
 
